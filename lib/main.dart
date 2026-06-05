e49e9e7b8c1b98c1317744d5444baa9226a82d37
ssonyii <sofyaandr2005@gmail.com>
2026-06-05 12:32:26 +0200

    test fix 5, need to modify later

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,26 @@ class _AppLoaderState extends State<_AppLoader> {
       theme: themeData,
       darkTheme: _buildTheme(TelegramThemeParams.dark),
       themeMode: tgTheme.isDark ? ThemeMode.dark : ThemeMode.light,
+      onGenerateRoute: (settings) {
+        final path = settings.name; // здесь будет лежать твой '/s/123'
+        
+        // Если кто-то зашел по ссылке, начинающейся с /s/
+        if (path != null && path.startsWith('/s/')) {
+          final slug = path.replaceFirst('/s/', ''); // достаем '123'
+          
+          return MaterialPageRoute(
+            builder: (context) {
+              // ВРЕМЕННАЯ ЗАГЛУШКА: Выводим номер на экран, чтобы доказать, что ссылки работают
+              return Scaffold(
+                body: Center(
+                  child: Text('Ура! Ссылка работает. Опрос: $slug', style: const TextStyle(fontSize: 24)),
+                ),
+              );
+            },
+          );
+        }
+        return null; 
+      },
       home: store.loaded ? const HomeScreen() : const _SplashScreen(),
     );
   }
